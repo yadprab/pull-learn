6b7b046046fb74d25cee5b28e289a34e81382bea
yathavan <yadhavanprabhakar744@gmail.com>
2021-05-10 12:24:37 +0530

    initial

---
+++ pullreq.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function pullreq() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+        deserunt sunt harum a expedita eos quia sint suscipit corrupti
+        dignissimos repellendus sit reprehenderit, ipsa pariatur. Ea a quis ex
+        officiis.
+      </p>
+    </>
+  );
+}
+
+export { pullreq };
